7f922d9e8d965e7c6256666f004bd06f18f12a6b
Andy Wright <atwright147@gmail.com>
2022-04-09 20:01:25 +0100

    implement button motifs

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,25 @@
 import { Component, Input } from '@angular/core';
 
+export enum Motifs {
+  primary = 'primary',
+  secondary = 'secondary',
+}
+
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
+  className = '';
+
   @Input() id = '';
   @Input() type = '';
   @Input() wide = false;
+  @Input() disabled = false;
+  // @Input() motif = Motifs.SECONDARY;
+  @Input() set motif(name: Motifs) {
+    console.info(name, Motifs[name]);
+    this.className = Motifs[name];
+  }
 }
--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ContactsService } from '../../services/contacts/contacts.service';
+import { Motifs } from '../button/button.component';
 
 @Component({
   selector: 'app-contact-details',
@@ -9,6 +10,7 @@ import { ContactsService } from '../../services/contacts/contacts.service';
 })
 export class ContactDetailsComponent implements OnInit {
   readonly selected$ = this.contactsService.selected$;
+  readonly Motifs = Motifs;
 
   constructor(
     private readonly contactsService: ContactsService,
