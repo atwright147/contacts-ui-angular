872bfbdad78a887ba6845f10c3ee33cacb58ff42
Andy Wright <atwright147@gmail.com>
2022-05-05 16:17:59 +0100

    fix linting errors

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,7 +1,4 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { LoginComponent } from './login.component';
